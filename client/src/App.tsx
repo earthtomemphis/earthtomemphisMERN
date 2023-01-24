563195163b60f1381c62eb5cb2326ca6e8c71c84
Memphis DiAngelis <earthtomemphis@gmail.com>
2023-01-24 00:54:36 -0700

    home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomePage from './pages/HomePage';
 
 const App = () => {
 	return (
@@ -9,7 +10,7 @@ const App = () => {
 			<Header />
 			<main className="py-3">
 				<Container>
-					<h1>earthtomemphis</h1>
+					<HomePage />
 				</Container>
 			</main>
 			<Footer />
