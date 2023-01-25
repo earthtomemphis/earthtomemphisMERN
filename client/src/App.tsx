f43fd06a9820f85166ce2dab4816943f53c122d5
Memphis DiAngelis <earthtomemphis@gmail.com>
2023-01-24 23:45:32 -0700

    product display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react';
 import { Container } from 'react-bootstrap';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 const App = () => {
 	return (
-		<>
+		<BrowserRouter>
 			<Header />
 			<main className="py-3">
 				<Container>
-					<HomePage />
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/product/:id" element={<ProductPage />} />
+					</Routes>
 				</Container>
 			</main>
 			<Footer />
-		</>
+		</BrowserRouter>
 	);
 };
 
