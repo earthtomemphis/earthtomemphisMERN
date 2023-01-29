942dd6b9be4ce6996e2789b0633d7e2a8c89c028
Memphis DiAngelis <earthtomemphis@gmail.com>
2023-01-28 22:51:58 -0700

    mongo setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
+import colors from 'colors';
 import products from './data/products.js';
+import connectDB from './config/db.js';
 
 config();
+
+connectDB();
 
 const app = express();
 
@@ -22,7 +26,7 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/products/:id', (req, res) => {
-	const product = find((product) => product._id === req.params.id);
+	const product = products.find((product) => product._id === req.params.id);
 	res.json(product);
 });
 
@@ -31,6 +35,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+			.underline.bold
 	)
 );
