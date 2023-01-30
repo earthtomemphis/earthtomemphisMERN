874f6c7f715c3093447f4b7afea26a9cddbd3893
Memphis DiAngelis <earthtomemphis@gmail.com>
2023-01-29 20:07:30 -0700

    custom error handling

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import { config } from 'dotenv';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
 import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 config();
 
@@ -22,6 +23,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
